cf562e85b8bdd883d72b9d06cada55b4cac39317
yaoguangluo <fengyue1985>
2019-02-14 17:09:46 +0800

    rest call set "/devopsCache" interface

--- DetaSrc/org/deta/boot/controller/DevopsController.java
+++ DetaSrc/org/deta/boot/controller/DevopsController.java
@@ -4,7 +4,7 @@ import org.lyg.common.maps.VtoV;
 import org.lyg.vpc.process.portImpl.DevopsStatus;
 public class DevopsController {
 	public static String exec(String string, Map<String, String> data) throws Exception {
-		if(string.equalsIgnoreCase("/")){
+		if(string.equalsIgnoreCase("/devopsCache")){
 			return VtoV.ObjectToJsonString(new DevopsStatus().getServerInf(data.get("token") ,data.get("email")
 					, data.get("password")));
 		}	
