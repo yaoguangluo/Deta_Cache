bc814cf9b0b631c4ba8fd83903c98098a6bbc92f
sofia <jarvisbert@foxmail.com>
2019-02-15 19:32:55 +1000

    version 1
    1. devopsCache接口调通； 2. 新增RSA非对称加密算法实现

---
+++ src/main/java/org/lyg/common/utils/RsaSecurity.java
@@ -0,0 +1,94 @@
+package org.lyg.common.utils;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+
+import static sun.security.x509.CertificateAlgorithmId.ALGORITHM;
+
+public class RsaSecurity {
+
+    private static int KEYSIZE=1024;
+
+    public static void main(String[] args) throws Exception {
+
+        generateKeyPair();
+        String source = "sofiabert";//要加密的字符串
+
+        String cryptograph = encrypt(source);//生成的密文
+        System.out.println("encrypt code--->"+cryptograph);
+
+        String target = decrypt(cryptograph);//解密密文
+        System.out.println("decrypt code--->"+target);
+
+
+    }
+
+    private static void generateKeyPair() throws Exception{
+        /** RSA算法要求有一个可信任的随机数源 */
+        SecureRandom sr = new SecureRandom();
+        /** 为RSA算法创建一个KeyPairGenerator对象 */
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        /** 利用上面的随机数据源初始化这个KeyPairGenerator对象 */
+        kpg.initialize(KEYSIZE, sr);
+        /** 生成密匙对 */
+        KeyPair kp = kpg.generateKeyPair();
+        /** 得到公钥 */
+        Key publicKey = kp.getPublic();
+        /** 得到私钥 */
+        Key privateKey = kp.getPrivate();
+        /** 用对象流将生成的密钥写入文件 */
+        ObjectOutputStream oos1 = new ObjectOutputStream(new FileOutputStream("publickey.keystore"));
+        ObjectOutputStream oos2 = new ObjectOutputStream(new FileOutputStream("privatekey.keystore"));
+        oos1.writeObject(publicKey);
+        oos2.writeObject(privateKey);
+        /** 清空缓存，关闭文件输出流 */
+        oos1.close();
+        oos2.close();
+    }
+
+    public static String encrypt(String source) throws Exception{
+
+        /** 将文件中的公钥对象读出 */
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("publickey.keystore"));
+        Key key = (Key) ois.readObject();
+        ois.close();
+        /** 得到Cipher对象来实现对源数据的RSA加密 */
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] b = source.getBytes();
+        /** 执行加密操作 */
+        byte[] b1 = cipher.doFinal(b);
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(b1);
+    }
+
+    public static String decrypt(String cryptograph) throws Exception{
+        /** 将文件中的私钥对象读出 */
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("privatekey.keystore"));
+        Key key = (Key) ois.readObject();
+        /** 得到Cipher对象对已用公钥加密的数据进行RSA解密 */
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] b1 = decoder.decodeBuffer(cryptograph);
+        /** 执行解密操作 */
+        byte[] b = cipher.doFinal(b1);
+        return new String(b);
+    }
+
+
+
+
+
+
+}
